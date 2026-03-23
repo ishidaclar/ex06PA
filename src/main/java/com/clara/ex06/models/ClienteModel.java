61f8534a5480eafe4d2280366fcb6bf605b22e6f
ishidaclar <cishidadias@gmail.com>
2026-03-23 18:36:59 -0300

    model criado

---
+++ src/main/java/com/clara/ex06/models/ClienteModel.java
@@ -0,0 +1,53 @@
+package com.clara.ex06.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_cliente")
+
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(name="E-mail")
+    private String email;
+    @Column(name="Telefone")
+    private String telefone;
+
+    public ClienteModel(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
